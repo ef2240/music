75f570a30bd0a25099a8f49e9a46f76f9acfbfd2
Eric Feder <efeder@theladders.com>
2015-08-21 17:08:08 -0400

    modify plot parameters and extract into function

--- Scripts/q3.R
+++ Scripts/q3.R
@@ -6,7 +6,7 @@ listen.details <- users[listen.details]
 listen.counts <- listen.details[!is.na(gender) & !is.na(age) & !is.na(genre), list(listens=sum(tracks_listened_to)), by=list(gender, age, genre)]
 
 # Remove users 60 or older (out of sample size concerns)
-listen.counts <- listen.counts[age < 60]
+listen.counts <- listen.counts[age <= 60]
 
 # Focus on top 10 genres
 genre.counts <- listen.counts[, list(total=sum(listens)), by=genre]
@@ -14,8 +14,8 @@ top10.genres <- genre.counts[order(total, decreasing=T)][1:10, genre]
 listen.counts.top10 <- listen.counts[genre %in% top10.genres]
 
 # Create stacked area charts
-male.chart <- ggplot(listen.counts.top10[gender == "MALE"][order(genre)], aes(x=age, y=listens))
-male.chart + geom_area(aes(fill=genre), position="fill")
-
-female.chart <- ggplot(listen.counts.top10[gender == "FEMALE"][order(genre)], aes(x=age, y=listens))
-female.chart + geom_area(aes(fill=genre), position="fill")
+createStackedAreaChart <- function(male){
+  chart <- ggplot(listen.counts.top10[gender == ifelse(male, "MALE", "FEMALE")][order(genre, decreasing=T)], aes(x=age, y=listens))
+  chart + geom_area(aes(fill=genre), position="fill") + theme(panel.grid=element_blank(), panel.background=element_blank(), axis.text.y=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_text(size=14, colour="black"), axis.ticks.x=element_line(colour="black")) + ggtitle(sprintf("%s Music Interests by Age", ifelse(male, "Male", "Female"))) + scale_y_continuous(expand=c(0, 0))
+}
+createStackedAreaChart(male=T)
