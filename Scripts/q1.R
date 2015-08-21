6d94bf30b340dc3a4fb042ca7c8d744482a4ac8d
Eric Feder <efeder@theladders.com>
2015-08-21 14:12:15 -0400

    add active as a user attribute instead

--- Scripts/q1.R
+++ Scripts/q1.R
@@ -1,6 +1,6 @@
 # Count number of users
 print(users[, .N])
 
-# Create dataset of active users and find length
-active.users <- users[users$profile_id %in% listens$profile_id]
-print(active.users[, .N])
+# Add flag to users indicating whether they are active or not
+users$active <- users$profile_id %in% listens$profile_id
+print(users[active == TRUE, .N])
