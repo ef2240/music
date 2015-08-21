d4a7a459962e29a8778db30dde38d4b769b16099
Eric Feder <efeder@theladders.com>
2015-08-21 19:20:08 -0400

    rollup everything outside top 6 into "Other"

--- Scripts/q3.R
+++ Scripts/q3.R
@@ -8,14 +8,16 @@ listen.counts <- listen.details[!is.na(gender) & !is.na(age) & !is.na(genre), li
 # Remove users 60 or older (out of sample size concerns)
 listen.counts <- listen.counts[age <= 60]
 
-# Focus on top 10 genres
+# Roll up long tail on genres into "Other"
 genre.counts <- listen.counts[, list(total=sum(listens)), by=genre]
-top10.genres <- genre.counts[order(total, decreasing=T)][1:10, genre]
-listen.counts.top10 <- listen.counts[genre %in% top10.genres]
+top.genres <- genre.counts[order(total, decreasing=T)][1:6, genre]
+levels(listen.counts$genre) <- c(levels(listen.counts$genre), "Other")
+listen.counts$genre[!listen.counts$genre %in% top.genres] <- "Other"
+listen.counts.rollup <- listen.counts[, list(listens=sum(listens)), by=list(genre, gender, age)]
 
 # Create stacked area charts
 createStackedAreaChart <- function(male){
-  chart <- ggplot(listen.counts.top10[gender == ifelse(male, "MALE", "FEMALE")][order(genre, decreasing=T)], aes(x=age, y=listens))
-  chart + geom_area(aes(fill=genre), position="fill") + theme(panel.grid=element_blank(), panel.background=element_blank(), axis.text.y=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_text(size=14, colour="black"), axis.ticks.x=element_line(colour="black")) + ggtitle(sprintf("%s Music Interests by Age", ifelse(male, "Male", "Female"))) + scale_y_continuous(expand=c(0, 0))
+  chart <- ggplot(listen.counts.rollup[gender == ifelse(male, "MALE", "FEMALE")][order(genre, decreasing=T)], aes(x=age, y=listens))
+  chart + geom_area(aes(fill=genre), position="fill") + theme(panel.grid=element_blank(), panel.background=element_blank(), axis.text.y=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_text(size=14, colour="black"), axis.ticks.x=element_line(colour="black"), plot.title=element_text(size=16, vjust=2)) + ggtitle(sprintf("%s Music Interests by Age", ifelse(male, "Male", "Female"))) + scale_y_continuous(expand=c(0, 0))
 }
 createStackedAreaChart(male=T)
